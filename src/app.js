6ad8afe4a522cd29d77be8870b844c54ddf11e3e
Jamie (Munro Research) <jamie@munro-research.com>
2025-02-13 17:54:10 +0000

    Basic user login and retrieve function

---
+++ src/app.js
@@ -0,0 +1,85 @@
+//2025 Munro Research Limited, All rights reserved
+
+const express = require('express');
+const bcrypt = require('bcrypt');
+const compression = require('compression')
+const bodyParser = require("body-parser");
+require("dotenv").config(); 
+
+const log = require("./log.js");
+const database = require("./database.js")
+
+var app = express();
+
+//response body compression
+app.use(compression())
+
+app.use(express.static('public-html'))
+
+//json middleware
+app.use(bodyParser.json({limit: process.env.MAX_PAYLOAD_BYTES}));
+
+//respond to healthcheck
+app.get('/', (req, res) => {
+    res.status(200).send();
+});
+
+app.post("/login", async (req, res) => {
+    try {
+        const { credentials } = req.body;
+        const { email, password } = credentials;
+
+        log.info(`Log in request from ${email}`)
+    
+        let user = await login(email, password);
+    
+        if (user.privilege) {
+            res.status(200).json({
+                privilege: user.privilege,
+            });
+        }
+        else throw new Error("User lacks permission");
+    } catch (err) {
+        log.error(err);
+        res.status(400).json({error: err.message});
+    }
+})
+
+app.post("/view-user", async (req, res) => {
+    try {
+        const { credentials, userEmail } = req.body;
+        const { email, password } = credentials;
+
+        log.info(`Log in request from ${email}`)
+    
+        let user = await login(email, password);
+    
+        if (user.privilege == "admin") {
+            let retrievedUser =  await database.getUser({email: userEmail.toLowerCase()});
+
+            if (retrievedUser) {
+                res.status(200).json({
+                    user: retrievedUser,
+                });
+            } else throw new Error("User not found");
+        }
+        else throw new Error("User lacks permission");
+    } catch (err) {
+        log.error(err);
+        res.status(400).json({error: err.message});
+    }
+})
+
+async function login(email, password) {
+    let user = await database.getUser({email: email.toLowerCase()});
+
+    if (user == undefined) throw new Error("Authentication Failed")
+
+    if (await bcrypt.compare(password, user.auth.hashedPassword)) {
+        return user;
+    } else throw new Error("Authentication Failed");
+}
+  
+app.listen(process.env.PORT, () => {
+    log.info(`Listening on port ${process.env.PORT}`);
+});
---
+++ src/public-html/index.js
@@ -0,0 +1,52 @@
+//2025 Munro Research Limited, All rights reserved
+
+var credentials = null;
+var privilege = null;
+
+async function login() {
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    let response = await fetch(`/login`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({credentials: {email, password}})
+    });
+
+    let json = await response.json();
+
+    if (response.status == 200) {
+        credentials = {email, password};
+        privilege = json.privilege;
+
+        document.getElementById("logged-out").style.display = "none";
+        document.getElementById("logged-in").style.display = "block";
+        document.getElementById("logged-in-msg").innerHTML = `Logged in as ${credentials.email} (${privilege})`;
+    } else {
+        alert(json.error);
+    }
+}
+
+async function viewUser() {
+    let userEmail = document.getElementById("user-email").value;
+
+    let response = await fetch(`/view-user`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({credentials, userEmail})
+    });
+
+    let json = await response.json();
+
+    if (response.status == 200) {
+        document.getElementById("user-json").innerHTML = JSON.stringify(json.user);
+    } else {
+        alert(json.error);
+    }
+}
