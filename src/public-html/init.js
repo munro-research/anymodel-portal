425ddc2bbf7beddcf3cfa060f29d6acc495d8b8c
Jamie (Munro Research) <jamie@munro-research.com>
2025-04-11 15:52:49 +0100

    Seperate init code from index

---
+++ src/public-html/init.js
@@ -0,0 +1,84 @@
+//2025 Munro Research Limited, All rights reserved
+
+const PREFIX = "portal"
+const LOG_OUT_URL = "/portal/";
+
+var credentials = null;
+var privilege = null;
+let account = null;
+var id = null;
+
+async function init() {
+    let item = localStorage.getItem('credentials');
+    if (item) {
+        let loadedCredentials = JSON.parse(item);
+        await processLogin(loadedCredentials.email, loadedCredentials.password);
+    }
+}
+
+async function postLogin() {
+    if (window.location.toString().endsWith(LOG_OUT_URL) || window.location.toString().includes("index.html")) {
+        initIndex();
+        return;
+    } 
+
+    if (window.location.toString().includes("graphs.html")) {
+        initGraphs();
+        return;
+    } 
+
+    if (window.location.toString().includes("affiliate.html")) {
+        initAffiliates();
+        return;
+    } 
+}
+
+async function logout() {
+    localStorage.removeItem('credentials');
+    window.location = LOG_OUT_URL;
+}
+
+async function login() {
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    let error = await processLogin(email, password);
+    if (error) {
+        alert(error);
+    }
+}
+
+async function processLogin(email, password) {
+    let response = await fetch(`/${PREFIX}/login`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({credentials: {email, password}})
+    });
+
+    let json = await response.json();
+
+    if (response.status == 200) {
+        credentials = {email, password};
+        privilege = json.privilege;
+        account = json.account;
+        id = json.id;
+
+        if (!privilege) privilege = "standard"
+
+        document.getElementById("logged-out").style.display = "none";
+        document.getElementById("logged-in").style.display = "block";
+        document.getElementById("logged-in-msg").innerHTML = `Logged in as ${credentials.email} (${privilege})`;
+
+        localStorage.setItem('credentials', JSON.stringify(credentials));
+
+        postLogin();
+
+        return null;
+    } else {
+        localStorage.removeItem('credentials');
+        return json.error;
+    }
+}
